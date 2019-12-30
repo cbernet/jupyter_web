92bde57b22282f4d87b4fd89c7606ce43adf5c03
Colin Bernet <colin.bernet@cern.ch>
2019-12-30 16:22:03 +0100

    now using jinja2

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This repository demonstrates how to perform a full integration of a jupyter notebook in a web page. 
 
-The code is highlighted with [pygments](https://pygments.org/). Math equations are rendered with [MathJax](https://www.mathjax.org/). The jupyter notebook is converted to HTML with [nbconvert](https://nbconvert.readthedocs.io/en/latest/). Custom CSS classes are provided fora  consistent style between the included jupyter HTML file, and the the mother HTML page. 
+The code is highlighted with [pygments](https://pygments.org/). Math equations are rendered with [MathJax](https://www.mathjax.org/). The jupyter notebook is converted to HTML with [nbconvert](https://nbconvert.readthedocs.io/en/latest/). The main style is from [bootstrap](https://getbootstrap.com/). Custom CSS classes are provided in this repo for a  consistent style between the included jupyter HTML file, and the mother HTML page. 
 
 ## Description of the repository
 
@@ -21,4 +21,16 @@ the [css](css) directory contains custom CSS classes for the styling of the vari
 * [css/main.css](css/main.css) : main styling 
 * [css/notebook.css](css/notebook.css) : styling of the jupyter notebook elements (e.g. input and output prompts, images, ...)
 * [css/pygments/notebook/colorful.css](css/pygments/notebook/colorful.css) : highlighting style for code in the jupyter notebook
+
+## Rendering the HTML file
+
+Just do
+
+```
+python render.py
+```
+
+And open the resulting `index.html` in your browser to see the results. 
+
+**If you edit `template.html` do not forget to render again.**
 
--- render.py
+++ render.py
@@ -1,49 +1,23 @@
 import os
-import re
-import pprint
 
-def process_lines(lines, command):
-    new_lines = ['<pre>']
-    new_lines.extend(lines)
-    new_lines.append('</pre>')
-    return new_lines
+from jinja2 import Environment, FileSystemLoader
 
-input_file = 'template.html'
+# this tells jinja2 to look for templates
+# in the templates subdirectory
+env = Environment(
+    loader = FileSystemLoader('templates'),
+)
+
+input_file = 'template_jinja.html'
 output_file = 'index.html'
 
-include_pattern = re.compile(r'^\s*\{\%\s+include\s+(\S+)\s+\%\}\s*$')
-begin_pattern = re.compile(r'^\s*\{\%\s+begin\s+(\S+)\s+\%\}\s*$')
-end_pattern = re.compile(r'^\s*\{\%\s+end\s+(\S+)\s+\%\}\s*$')
+# reading the template
+template = env.get_template(input_file)
+# render the template
+# in other words, we replace the template tag
+# by the contents of the overfitting file
+rendered = template.render()
 
-lines_to_process = []
-processing_lines = False
-with open(output_file, 'w') as outfile:
-    with open(input_file) as infile:
-        for line in infile.readlines():
-            m = include_pattern.match(line)
-            if m:
-                include_file = m.group(1)
-                with open(include_file) as incfile:
-                    outfile.writelines(incfile.readlines())
-                continue
-            m = begin_pattern.match(line)
-            if m:
-                command = m.group(1)
-                processing_lines = True
-                lines_to_process = []
-                continue
-            m = end_pattern.match(line)
-            if m:
-                processing_lines = False
-                processed_lines = process_lines(lines_to_process,
-                                                command)
-                pprint.pprint(processed_lines)
-                outfile.writelines(processed_lines)
-                continue
-            if processing_lines:
-                lines_to_process.append(line)
-                continue
-
-            outfile.write(line)
-
-        
+# write the result to disk in index.html
+with open(output_file, 'w') as ofile:
+    ofile.write(rendered)
